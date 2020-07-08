2fbac9274a1db6a921fedf1ea2316b89e9099de7
serdoo <61329458+serdoo@users.noreply.github.com>
2020-07-08 12:13:22 +0300

    Add files via upload

---
+++ srdm/scripts/main.js
@@ -0,0 +1,41 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/Samsung_logo_blue.png') {
+      myImage.setAttribute ('src','images/2.png');
+    } 
+    else {
+      myImage.setAttribute ('src','images/2.png');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'do what you can not, ' +  myName;
+  }
+
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'do what you can not, ' + storedName;
+  }
+
+  myButton.onclick = function() {
+    setUserName();
+  }
+
+  function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.innerHTML = 'do what you can not, ' + myName;
+    }
+  }
